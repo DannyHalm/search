70b45c0ae0c3e1e13259ffb68fe57ad7ea98563a
Daniel Halmstrand <johndoe@example.comdaniel.halmstrand@gmail.com>
2022-05-23 22:32:54 +0100

    cleaning structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,9 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MainComponent } from './components/main/main.component';
 import { SearchComponent } from './components/search/search.component';
 
 const routes: Routes = [
-  {path:"", component:MainComponent},
-  {path:"search", component:SearchComponent},
-  {path:"searchBody", component:Component}
+  {path:"", component:SearchComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,15 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainComponent } from './components/main/main.component';
 import { SearchComponent } from './components/search/search.component';
-import { SearchBodyComponent } from './components/search-body/search-body.component';
 import { SearchService } from './services/search.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent,
     SearchComponent,
-    SearchBodyComponent
+
   ],
   imports: [
     BrowserModule,
