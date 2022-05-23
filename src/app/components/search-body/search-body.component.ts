e6b045a524944ed4aeb93dc8c2aece5ee8429e83
Daniel Halmstrand <johndoe@example.comdaniel.halmstrand@gmail.com>
2022-05-23 18:15:20 +0100

    migrating to scss

--- src/app/components/search-body/search-body.component.ts
+++ src/app/components/search-body/search-body.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-search-body',
   templateUrl: './search-body.component.html',
-  styleUrls: ['./search-body.component.css']
+  styleUrls: ['./search-body.component.scss']
 })
 export class SearchBodyComponent implements OnInit {
 
--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -8,7 +8,7 @@ import { SearchService } from 'src/app/services/search.service';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
       searchSubject$ = new Subject<string>();
